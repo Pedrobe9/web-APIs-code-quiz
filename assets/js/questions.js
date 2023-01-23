6283b96ea2b672ea53188d13493c1346fba2ff15
Pedrobe <109363992+Pedrobe9@users.noreply.github.com>
2023-01-23 00:27:21 +0000

    Adding complete set of questions and answers to questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,12 +4,12 @@ var questions = [
     "Valid data types to be used in JSON are ____.", 
     "Which JavaScript method will not display data on the screen?",
     "Which symbols are used to signal comments in JavaScript?",
-    "Which statement is false about variables in JS",
-    "Which method does NOT extract a part of a string",
-    "",
-    "",
-    "",
-    ""];
+    "Which statement is false about variables in JS:",
+    "Which method does NOT extract a part of a string?",
+    "Functions are declared with the following syntax:",
+    "A function defined as the property of an object, is called ____.",
+    "Which statement is false about JSON syntax:",
+    "Which statement is NOT considered best practice?"];
 
 var multAnswers = [
 ["String", "Number", "Boolean", "Alert"],
@@ -21,8 +21,14 @@ var multAnswers = [
 "Using a 'let' variable outside the function where it is declared is allowed.", 
 "Variables defined with 'let' have Block Scope."],
 ["concat()", "slice(start, end)", "substring(start, end)", "substr(start, length)"],
-["", "", "", ""],
-["", "", "", ""],
-["", "", "", ""],
-["", "", "", ""]
+["function functionName(parameters) (// code to be executed)", 
+"function functionName(code to be executed) [// parameters]", 
+"function functionName(parameters) {// code to be executed}", 
+"function functionName(parameters) [// code to be executed]"], 
+["object constructor", "method to the object", 
+"objective property", "object function"],
+["Data is in name/value pairs.", "Data is separated by a semicolon.", 
+"Curly braces hold objects.", "Square brackets hold arrays."],
+["Initialize Variables.", "Put all declarations at the top of each script or function.", 
+"Use == for comparison.", "Always end your switch statements with a default."]
 ]
