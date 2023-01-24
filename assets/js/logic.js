fb09666f28909740df92fe3a0c0b1c12e4fa546c
Pedrobe <109363992+Pedrobe9@users.noreply.github.com>
2023-01-24 00:04:37 +0000

    Re-structure and correction of main functions. New functions created.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,118 +6,172 @@ var startScreen = document.querySelector("#start-screen");
 var startButton = document.querySelector("#start");
 var questionElement = document.querySelector("#question-title");
 var choicesElement = document.querySelector("#choices");
+// https://www.w3schools.com/JSREF/prop_node_childnodes.asp
+var choicesListElement = choicesElement.childNodes;
 var finalScoreSpan = document.querySelector("#final-score");
 var initialsInput = document.querySelector("#initials");
 var displayQuestions = document.querySelector("#questions");
 var displayElement = document.querySelector(".hide");
-
+var pElement = document.createElement("p");
 
 // Use file questions.js to store the questions and answers as seein g in
 // https://stackoverflow.com/questions/41255861/how-to-pass-variable-from-one-javascript-to-another-javascript-file
 
 
 // global variables
-var rightQuestion = false;
+var wrongQuestion = false;
+var score = 0;
 var timerCount;
-var questionChoice = question;
-var multAnswerChoice = multAnswer;
-var rightAnswer = rightAnswers
-
-
-// Call startQuiz function with a click -associate event listener to start button
-startButton.addEventListener("click", startQuiz);
-
-function startQuiz() {
-    startScreen.innerHTML = "";
-    rightQuestion = false;
-    timerCount = 200;
-    // Deactivate startButton to prevent
-    startButton.disabled = true;
-    displayQuiz();
-    startTimer();
-}
+var indexQuestion = 0;
+var timer;
+var indexQ;
+var answers;
+var answerList;
 
 
 
 // start timer and check if quiz is right
 function startTimer() {
     timer = setInterval(function() {
-      timerCount--;
-      timerElement.textContent = timerCount;
-      if (timerCount >= 0) {
-        // if question right in time set, Clears interval and stops timer
-        if (rightQuestion && timerCount > 0) {
-          clearInterval(timer);
-          winQuiz();
+        // Time available decreases
+        timerCount--;
+        if (timerCount < 0) {
+            timerCount = 0;
         }
-      }
-      // Tests if time has run out, clears interval and triggers loseQuiz function
-      if (timerCount === 0) {
-        // Clears interval
-        clearInterval(timer);
-        loseQuiz();
-      }
+        timerElement.textContent = timerCount;
+        if (timerCount > 0) {
+            // if question right in time set, Clears interval
+            if (wrongQuestion) {
+                timerCount -= 10;
+                
+            }
+            wrongQuestion = false;
+        } else {
+            clearInterval(timer);
+            endQuiz()
+        }
+      
     }, 1000);
 }
 
-function displayQuiz() {
-    var index = Math.floor(Math.random() * questionChoice.length);
+function startQuiz() {
+    // Hide the starting screen so the questions can be seeing.
+    //startScreen.innerHTML = "";
+    document.getElementById("start-screen").className = "hide";
+    rightQuestion = false;
+    timerCount = 60;
+    // Deactivate startButton to prevent click
+    startButton.disabled = true;
+    const index = Math.floor(Math.random() * question.length);
     document.getElementById("questions").className = "MyClass";
+    console.log("init: ", index);
+    indexQ = index;;
+    startTimer();
+    displayQuiz(indexQ);
+}
 
-    for (let i = 0; i < questionChoice.length; i++) {
-        index = index + i;
-        if (timerCount >= 0) {
-            if (index < questionChoice.length) {
-                index = index;
-            } else {
-                index = index - questionChoice.length;
-            }
-            document.getElementById("questions").className = "myClass";
-            //questionElement.textContent = "";
-            questionElement.textContent = questionChoice[index];
-            //choicesElement.textContent = multAnswerChoice[index];
-            var multAnswer = multAnswerChoice[i];
-            for (let i = 0; i < multAnswer.length; i++) {
-                
-                var li = document.createElement("li");
-                //li.textContent = multAnswer;
-                li.setAttribute("data-index", i);
-            
-                var button = document.createElement("button");
-                button.textContent = multAnswer[i];
-            
-                li.appendChild(button);
-                choicesElement.appendChild(li);
-            }
-
-            choicesElement.addEventListener("click", function(event) {
-                var element = event.target;
-              
-                // If that element is a button...
-                if (element.matches("button") === true) {
-                  // Get its data-index value and remove the todo element from the list
-                  var index = element.parentElement.getAttribute("data-index");
-                  todos.splice(index, 1);
-              
-                  // Store updated todos in localStorage, re-render the list
-                  storeTodos();
-                  renderTodos();
-                }
-              });
-            console.log(questionChoice[index], "-----", multAnswerChoice[index]);
-        }
+function displayQuiz(iQ) {
+    // Change hide class to show the questions
+    document.getElementById("questions").className = "myClass";
+    //questionElement.textContent = "";
+    questionElement.textContent = question[iQ];
+    var answer = multAnswer[iQ];
+    console.log(question[iQ], "---", answer);
+    console.log("after if: ", iQ);
+    for (let i = 0; i < 4; i++) {
         
+        var li = document.createElement("li");
+        //li.textContent = multAnswer;
+        li.setAttribute("data-index", i);
+    
+        var button = document.createElement("button");
+        button.textContent = answer[i];
+    
+        li.appendChild(button);
+        choicesElement.appendChild(li);
+    
+    }
+    clickAnswer();
+}
+
+function rightMessage() {
+    pElement.textContent = "Right Answer";
+    choicesElement.appendChild(pElement);
+}
+function wrongMessage() {
+    pElement.textContent = "Wrong Answer";
+    choicesElement.appendChild(pElement);
+}
+function clickAnswer() {
+    // Check for click in list
+    var choicesListElement = choicesElement.childNodes;
+    var answer = multAnswer[indexQ];
+    for (let i = 0; i < answer.length; i++) {
+        choicesListElement[i].addEventListener("click", function(event) {
+            var answerClick = event.target;
+            answerList = answerClick.textContent;
+            // Get its data-index value and remove the todo element from the list
+            //var indexData = choicesListElement[i].parentElement.getAttribute("data-index");
+            console.log("answerList: ", answerList, "rightAnswers[indexQ]: ", rightAnswers[indexQ]);
+            if (answerList === rightAnswers[indexQ]) {
+                rightMessage();
+                /* Play audio as shown in https://dobrian.github.io/cmp/topics/sample-recording-and-
+                playback-with-web-audio-api/1.loading-and-playing-sound-files.html#:~:
+                text=Method%202%3A%20JavaScript,back%20the%20sound%20with%20the%20.*/
+                const audio = new Audio("./assets/sfx/correct.wav");
+                audio.play()
+                questionElement.textContent = "";
+                choicesElement.textContent = "";
+                //pElement.textContent = "";
+                score++;
+                indexQ++;
+                if (timerCount >= 0) {
+                    if (indexQ < question.length) {
+                        indexQ = indexQ;
+                    } else {
+                        indexQ = indexQ - question.length;
+                    }
+                    displayQuiz(indexQ);
+                }   
+                
+
+            } else {
+                wrongMessage();
+                const audio = new Audio("./assets/sfx/incorrect.wav");
+                audio.play()
+                questionElement.textContent = "";
+                choicesElement.textContent = "";
+                //pElement.textContent = "";
+                wrongQuestion = true;
+                indexQ++;
+                if (timerCount >= 0) {
+                    if (indexQ < question.length) {
+                        indexQ = indexQ;
+                    } else {
+                        indexQ = indexQ - question.length;
+                    }
+                    displayQuiz(indexQ);
+                }   
+                
+            }
+        });
     }
     
-
 }
 
 
-// When timer reaches 0, loseQuiz function is called.
+// When timer reaches 0, endQuiz function is called.
 // Start button is activated again.
-function loseQuiz() {
-    wordBlank.textContent = "GAME OVER";
-    loseCounter++
+function endQuiz() {
+    console.log("Score: ", score)
     startButton.disabled = false;
-    
+    document.getElementById("start-screen").className = "start";
+    //startScreen.innerHTML = "GAME OVER";
+    //startScreen.innerHTML = "Score: ";
+    //startScreen.innerHTML = score;
   }
+
+
+  // Call startQuiz function with a click -associate event listener to start button
+startButton.addEventListener("click", startQuiz);
+console.log("passed");
